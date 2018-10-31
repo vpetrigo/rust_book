4263de4cdc12e5e4e8b0dfcbd09d50f8b49199f6
Vladimir Petrigo <taenaru@gmail.com>
2018-10-31 18:28:32 +0300

    rectangle: Implement simple rectangle struct

---
+++ rectangle/src/main.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        (self.width >= other.width && self.height >= other.height) ||
+            (self.width >= other.height && self.height >= other.width)
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 40 };
+    let rect2 = Rectangle { width: 35, height: 23};
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect2 hold rect1? {}", rect2.can_hold(&rect1));
+    println!("Rect1: {:#?}", rect1);
+    println!("Rect2: {:#?}", rect2);
+}
