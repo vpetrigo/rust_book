f29f0bd859f86aef5e10aaafa9e633881a4d6238
Vladimir Petrigo <taenaru@gmail.com>
2018-11-23 03:14:34 +0300

    blog: Add proper interface

---
+++ blog/src/lib.rs
@@ -0,0 +1,122 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        if let Some(s) = self.state.as_ref() {
+            self.content.push_str(s.add_text(text))
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(&self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+
+    pub fn reject(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.reject());
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+
+    fn content<'a>(&self, _post: &'a Post) -> &'a str {
+        ""
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State>;
+
+    fn add_text<'a>(&self, _text: &'a str) -> &'a str {
+        ""
+    }
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview { approve_counter: 0 })
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn add_text<'a>(&self, text: &'a str) -> &'a str {
+        text
+    }
+}
+
+struct PendingReview {
+    approve_counter: u8
+}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(mut self: Box<Self>) -> Box<dyn State> {
+        println!("Pending Review cnt: {}", self.approve_counter);
+
+        if self.approve_counter + 1 != 2 {
+            self.approve_counter += 1;
+            return self;
+        }
+
+        Box::new(Published {})
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Draft {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
