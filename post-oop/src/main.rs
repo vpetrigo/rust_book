53413b6e4ff275d8c7d1af30fd56fa920189cc6c
Vladimir Petrigo <taenaru@gmail.com>
2018-11-24 17:28:39 +0300

    post-oop: Add simple main app

---
+++ post-oop/src/main.rs
@@ -0,0 +1,19 @@
+extern crate blog;
+
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    post.add_text(" and tomorrow");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.add_text(" and yesterday");
+    post.approve();
+    post.approve();
+    assert_eq!("I ate a salad for lunch today and tomorrow", post.content());
+}
