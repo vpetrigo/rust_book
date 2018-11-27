1dcaa934b8f01460d243ac63d7d8b9c36d3a0c93
Vladimir Petrigo <taenaru@gmail.com>
2018-11-27 15:29:52 +0300

    unsafe: Add another mutable pointer

--- unsafe/src/main.rs
+++ unsafe/src/main.rs
@@ -1,17 +1,22 @@
 fn main() {
     let mut x = 10;
     let y = &mut x as *mut i32;
+    let k = &mut x as *mut i32;
     let z = &x as *const i32;
 
     unsafe {
         println!("Mutable ptr: {}", *y);
+        println!("Mutable ptr: {}", *k);
         println!("Immutable ptr: {}", *z);
         *y = 15;
         println!("Mutable ptr: {}", *y);
+        println!("Mutable ptr: {}", *k);
         println!("Immutable ptr: {}", *z);
 
         *y = *y + *z;
+        *k = 24;
         println!("Mutable ptr: {}", *y);
+        println!("Mutable ptr: {}", *k);
         println!("Immutable ptr: {}", *z);
     }
 
